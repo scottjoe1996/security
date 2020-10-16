e73fa91e49b9dc2dfe7330821c6fb8f5cb6dc87f
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-10-16 16:52:19 +0100

    added restTemplateResponseErrorHandler tests

--- src/test/java/com/postitapplications/security/exception/RestTemplateResponseErrorHandlerTests.java
+++ src/test/java/com/postitapplications/security/exception/RestTemplateResponseErrorHandlerTests.java
@@ -1,7 +1,89 @@
 package com.postitapplications.security.exception;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.postitapplications.exception.exceptions.ExternalServiceException;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.mock.http.client.MockClientHttpResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 class RestTemplateResponseErrorHandlerTests {
 
+    @Test
+    public void hasErrorShouldReturnTrueIfHttpResponseHas4XXStatusCode() throws IOException {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.NOT_FOUND);
+
+        boolean result = errorHandler.hasError(clientHttpResponse);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void hasErrorShouldReturnTrueIfHttpResponseHas5XXStatusCode() throws IOException {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.BAD_GATEWAY);
+
+        boolean result = errorHandler.hasError(clientHttpResponse);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void hasErrorShouldReturnFalseIfHttpResponseHasNeither4xxOr5xxStatusCode()
+        throws IOException {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.OK);
+
+        boolean result = errorHandler.hasError(clientHttpResponse);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void handleErrorShouldThrowExternalServiceExceptionWithNotFoundStatusCode() {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.NOT_FOUND);
+
+        ExternalServiceException exception = assertThrows(ExternalServiceException.class, () -> {
+            errorHandler.handleError(clientHttpResponse);
+        });
+
+        assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void handleErrorShouldThrowExternalServiceExceptionWithBadGatewayStatusCode() {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.BAD_GATEWAY);
+
+        ExternalServiceException exception = assertThrows(ExternalServiceException.class, () -> {
+            errorHandler.handleError(clientHttpResponse);
+        });
+
+        assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_GATEWAY);
+    }
+
+    @Test
+    public void handleErrorShouldNotThrowExternalServiceException() {
+        RestTemplateResponseErrorHandler errorHandler = new RestTemplateResponseErrorHandler();
+        ClientHttpResponse clientHttpResponse = new MockClientHttpResponse(new byte[0],
+            HttpStatus.OK);
+
+        assertDoesNotThrow(() -> errorHandler.handleError(clientHttpResponse));
+    }
 }
